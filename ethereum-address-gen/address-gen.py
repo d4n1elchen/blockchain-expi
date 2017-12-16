e7af45c8eec67914e2de0f982a3661c7979e47de
Daniel Chen <team6612@gmail.com>
2017-12-16 17:28:47 +0800

    update

--- ethereum-address-gen/address-gen.py
+++ ethereum-address-gen/address-gen.py
@@ -2,6 +2,7 @@ from ecdsa import SECP256k1, SigningKey
 from ecdsa.util import randrange_from_seed__trytryagain
 import hashlib
 import sha3
+import secrets
 
 def make_key(seed):
   secexp = randrange_from_seed__trytryagain(seed, SECP256k1.order)
@@ -13,10 +14,17 @@ def toEthAddress(pk):
 	address = keccak.hexdigest()[24:]
 	return address
 
-seed = hashlib.sha256("ABCDEF12".encode('utf-8')).digest()
-priv = make_key(seed)
-pub = priv.get_verifying_key()
+desired_str = "dc"
+found = False
 
-print("Private: "+priv.to_string().hex())
-print("Public:  "+pub.to_string().hex())
-print("Address: "+toEthAddress(pub))
+while not found:
+	seed = hashlib.sha256(secrets.token_bytes()).digest()
+	priv = make_key(seed)
+	pub = priv.get_verifying_key()
+	
+	if desired_str in priv.to_string().hex():
+		print("Private: "+priv.to_string().hex())
+		print("Public:  "+pub.to_string().hex())
+		print("Address: "+toEthAddress(pub))
+
+		found = True
